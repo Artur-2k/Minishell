630030db8e03bc72ff2d236b607ee15908ca2aac
Artur Silva <artuda-s@c2r4s11.42porto.com>
2024-11-21 13:43:02 +0000

    split tokens.c normi

--- src/tokenizer/split_tokens.c
+++ src/tokenizer/split_tokens.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split_tokens.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: artuda-s < artuda-s@student.42porto.com    +#+  +:+       +#+        */
+/*   By: artuda-s <artuda-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 17:50:23 by artuda-s          #+#    #+#             */
-/*   Updated: 2024/10/30 17:23:41 by artuda-s         ###   ########.fr       */
+/*   Updated: 2024/11/21 13:42:48 by artuda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,30 @@
  * @param str The string which will count tokens
  * @return The number of tokens
 */
-static int  ft_count_tokens(char *str)
+static int	ft_count_tokens(char *str)
 {
 	char	quote_char;
-    int     count;
-    int     i;
+	int		count;
+	int		i;
 
-    count = 0;
-    i = 0;
-    if (str[i])
-        count = 1;
-    while(str[i])
-    {
-        if (str[i] == '\'' || str[i] == '"')
-        {
-            quote_char = str[i++];
-            while (str[i] && str[i] != quote_char)
-                i++;
-        }
-        if (str[i] && str[i] == ' ' && str[i + 1])
+	count = 0;
+	i = 0;
+	if (str[i])
+		count = 1;
+	while (str[i])
+	{
+		if (str[i] == '\'' || str[i] == '"')
+		{
+			quote_char = str[i++];
+			while (str[i] && str[i] != quote_char)
+				i++;
+		}
+		if (str[i] && str[i] == ' ' && str[i + 1])
 			count++;
-        if (str[i])
-            i++;
-    }
-    return (count);
+		if (str[i])
+			i++;
+	}
+	return (count);
 }
 
 /*
@@ -55,19 +55,20 @@ static int  ft_count_tokens(char *str)
 */
 static int	ft_token_lenght(char *str)
 {
-	int len;
+	int		len;
+	char	quote_char;
 
 	len = 0;
 	while (str[len] && str[len] != ' ')
 	{
-        if (str[len] == '\'' || str[len] == '"')
-        {
-            char quote_char = str[len++];
-            while (str[len] && str[len] != quote_char)
-                len++;
-        }
-        if (str[len])
-            len++;
+		if (str[len] == '\'' || str[len] == '"')
+		{
+			quote_char = str[len++];
+			while (str[len] && str[len] != quote_char)
+				len++;
+		}
+		if (str[len])
+			len++;
 	}
 	return (len);
 }
@@ -79,19 +80,19 @@ static int	ft_token_lenght(char *str)
  * @param start The index of the starting char
  * @return The allocated extracted token. In case of error returns NULL
 */
-static char *ft_populate_tkn_arr(char *str, int *start)
+static char	*ft_populate_tkn_arr(char *str, int *start)
 {
-    int         tkn_len;
-    char        *ret;
+	int		tkn_len;
+	char	*ret;
 
-    if (str[*start] == ' ')
-        (*start)++;
-    tkn_len = ft_token_lenght(&str[*start]);
-    ret = ft_substr(str, *start, tkn_len);
-    if (!ret)
-        return (NULL);
-    *start = *start + tkn_len;
-    return (ret);
+	if (str[*start] == ' ')
+		(*start)++;
+	tkn_len = ft_token_lenght(&str[*start]);
+	ret = ft_substr(str, *start, tkn_len);
+	if (!ret)
+		return (NULL);
+	*start = *start + tkn_len;
+	return (ret);
 }
 
 /*
@@ -102,29 +103,28 @@ static char *ft_populate_tkn_arr(char *str, int *start)
  * @return Returns a NULL terminated string array
  * with a token for each string. Return NULL if any error occurs
 */
-char    **ft_split_tokens(char *str)
+char	**ft_split_tokens(char *str)
 {
-    char    **token_arr;
-    int     no_tokens;
-    int     i;
-    int     start;
+	char	**token_arr;
+	int		no_tokens;
+	int		i;
+	int		start;
 
-    if (!str || !*str)
-        return (NULL);
-    no_tokens = ft_count_tokens(str); // no of tokens to be allocated
-    token_arr = (char **)malloc(sizeof(char *) * (no_tokens + 1)); // null terminate the array too!
+	if (!str || !*str)
+		return (NULL);
+	no_tokens = ft_count_tokens(str);
+	token_arr = (char **)malloc(sizeof(char *) * (no_tokens + 1));
 	if (!token_arr)
 		return (ft_putstr_fd("Malloc error, sir\n", 2), NULL);
-
-    i = 0;
-    start = 0;
-    while (i < no_tokens)
-    {
-        token_arr[i] = ft_populate_tkn_arr(str, &start); // fetches the token
-        if (!token_arr[i])
-            return (ft_free_str_arr(token_arr), NULL);
-        i++;
-    }
-    token_arr[i] = NULL; // null terminates
-    return (token_arr);
+	i = 0;
+	start = 0;
+	while (i < no_tokens)
+	{
+		token_arr[i] = ft_populate_tkn_arr(str, &start);
+		if (!token_arr[i])
+			return (ft_free_str_arr(token_arr), NULL);
+		i++;
+	}
+	token_arr[i] = NULL;
+	return (token_arr);
 }
