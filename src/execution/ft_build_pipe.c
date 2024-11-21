b167a6fe4667d64c2f6357c1d4ca9edbff54b034
Artur Silva <artuda-s@c2r4s11.42porto.com>
2024-11-21 15:40:29 +0000

    ft_build_pipe.c norm ok

--- src/execution/ft_build_pipe.c
+++ src/execution/ft_build_pipe.c
@@ -6,7 +6,7 @@
 /*   By: artuda-s <artuda-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 15:19:00 by artuda-s          #+#    #+#             */
-/*   Updated: 2024/11/01 16:43:57 by artuda-s         ###   ########.fr       */
+/*   Updated: 2024/11/21 15:39:43 by artuda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,17 @@
  * that are missing to append to the tree
  * @return Returns the right side of the pipe or NULL in case of error
 */
-t_cmd   *ft_build_pipe(t_cmd *cmd, t_tokens **tkns ,t_shell *shell)
+t_cmd	*ft_build_pipe(t_cmd *cmd, t_tokens **tkns ,t_shell *shell)
 {
-    t_pipe  *pipe;
+	t_pipe	*pipe;
 
-    pipe = (t_pipe *)malloc(sizeof(t_pipe));
-    if (!pipe) // Malloc error
-        return (NULL);
-    pipe->type = PIPE;
-    pipe->left = cmd;
-    pipe->right = ft_build(tkns, shell);
-    if (!pipe->right)
-    {
-        return (free(pipe), NULL);
-    }
-    return ((t_cmd *)pipe);
+	pipe = (t_pipe *)malloc(sizeof(t_pipe));
+	if (!pipe)
+		return (NULL);
+	pipe->type = PIPE;
+	pipe->left = cmd;
+	pipe->right = ft_build(tkns, shell);
+	if (!pipe->right)
+		return (free(pipe), NULL);
+	return ((t_cmd *)pipe);
 }
