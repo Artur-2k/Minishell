6d70dafbbd5d4a12138e97e46a6332524cb92c0a
Artur Silva <artuda-s@c2r4s11.42porto.com>
2024-11-21 14:57:09 +0000

    main.c norm ok

--- src/minishell.c
+++ src/minishell.c
@@ -6,69 +6,54 @@
 /*   By: artuda-s <artuda-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 15:33:37 by artuda-s          #+#    #+#             */
-/*   Updated: 2024/11/21 13:18:12 by artuda-s         ###   ########.fr       */
+/*   Updated: 2024/11/21 14:55:02 by artuda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-extern unsigned char g_signal;
+extern unsigned char	g_signal;
 
-int     main(int ac, char** av, char *envp[])
+static void	ft_norminnete(t_shell *shell)
 {
-    t_shell    shell;
+	while (true)
+	{
+		g_signal = 0;
+		shell->input = readline(RED"Minihell => "RES);
+		if (shell->input == NULL)
+			break ;
+		if (g_signal)
+		{
+			shell->exit_status = 128 + g_signal;
+			free(shell->sexit_status);
+			shell->sexit_status = ft_itoa(shell->exit_status);
+		}
+		if (ft_tokenizer(shell))
+		{
+			add_history(shell->input);
+			shell->exit_status = 1;
+			free(shell->input);
+			continue ;
+		}
+		ft_run_cmd(shell);
+		if (*shell->input != '\0')
+			add_history(shell->input);
+		free(shell->input);
+	}
+}
 
-	// Initializing everything
+int	main(int ac, char **av, char *envp[])
+{
+	t_shell	shell;
+
 	if (ft_init_everything(&shell, envp, ac, av))
 		return (ft_putstr_fd("Malloc error, sir\n", STDERR_FILENO), 1);
-
-	// Read -> Parse -> Execute -> Repeat
-    while (true)
-    {
-        // Ctrl C
-        g_signal = 0;
-
-		// Reads input from user
-        shell.input = readline(RED"Minihell => "RES);
-
-        // Ctrl+D (EOF), readline retorna NULL
-        if (shell.input == NULL)
-            break;
-
-        if (g_signal)
-        {
-            shell.exit_status = 128 + g_signal;
-            free(shell.sexit_status);
-            shell.sexit_status = ft_itoa(shell.exit_status);
-        }
-
-
-        // Tokenizes the input and checks for syntax errors
-        if (ft_tokenizer(&shell))
-        {
-            add_history(shell.input);
-            shell.exit_status = 1;
-            free(shell.input);
-            continue ;
-        }
-
-        // This function doesnt need a return statement
-        ft_run_cmd(&shell);
-
-        // Verifica se o input não está vazio antes de adicionar ao histórico
-	    if (*shell.input != '\0')
-            add_history(shell.input);
-
-        free(shell.input); // input
-    }
-
-    //release everything
-    // envp, tree , history
-    free (shell.spid);
-    free (shell.sexit_status);
-    ft_free_envp_lst(shell.envp2lol_h); // envp list
-    ft_free_envp_lst(shell.my_envp_h); // envp list
-    rl_clear_history(); // history
-    printf("exit\n");
-    return (0);
+	ft_norminnete(&shell);
+	free(shell.spid);
+	free(shell.sexit_status);
+	ft_free_envp_lst(shell.envp2lol_h);
+	ft_free_envp_lst(shell.my_envp_h);
+	rl_clear_history();
+	printf("exit\n");
+	return (0);
 }
