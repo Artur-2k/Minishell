e91f3273182fedd452b0db1ac316e34f3d64585f
Davi Melo Cardozo <dmelo-ca@c2r3s11.42porto.com>
2024-11-21 15:45:39 +0000

    Normi em mais alguns do execution

--- src/execution/ft_build_pipe.c
+++ src/execution/ft_build_pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_build_pipe.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: artuda-s <artuda-s@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: dmelo-ca <dmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 15:19:00 by artuda-s          #+#    #+#             */
-/*   Updated: 2024/11/01 16:43:57 by artuda-s         ###   ########.fr       */
+/*   Updated: 2024/11/21 15:41:01 by dmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,24 @@
 
 /*
  * @brief Allocates memory for a new pipe node and sets the exec cmd to the left
- * and to the right recursivly calls the build functio with a pointer to the tokens
+ * and to the right recursivly calls the build
+ * functio with a pointer to the tokens
  * that are missing to append to the tree
  * @return Returns the right side of the pipe or NULL in case of error
 */
-t_cmd   *ft_build_pipe(t_cmd *cmd, t_tokens **tkns ,t_shell *shell)
+t_cmd	*ft_build_pipe(t_cmd *cmd, t_tokens **tkns ,t_shell *shell)
 {
-    t_pipe  *pipe;
+	t_pipe  *pipe;
 
-    pipe = (t_pipe *)malloc(sizeof(t_pipe));
-    if (!pipe) // Malloc error
-        return (NULL);
-    pipe->type = PIPE;
-    pipe->left = cmd;
-    pipe->right = ft_build(tkns, shell);
-    if (!pipe->right)
-    {
-        return (free(pipe), NULL);
-    }
-    return ((t_cmd *)pipe);
+	pipe = (t_pipe *)malloc(sizeof(t_pipe));
+	if (!pipe) // Malloc error
+		return (NULL);
+	pipe->type = PIPE;
+	pipe->left = cmd;
+	pipe->right = ft_build(tkns, shell);
+	if (!pipe->right)
+	{
+		return (free(pipe), NULL);
+	}
+	return ((t_cmd *)pipe);
 }
