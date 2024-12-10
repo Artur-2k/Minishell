1632e07851fa75ae042611eedef592b2ebdf45bc
Artur Silva <artuda-s@c2r1s11.42porto.com>
2024-12-10 19:30:41 +0000

    fixed export key  when it existed and null dereferenciation

--- src/built-ins/ft_export_helpers.c
+++ src/built-ins/ft_export_helpers.c
@@ -6,7 +6,7 @@
 /*   By: artuda-s <artuda-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 22:30:20 by artuda-s          #+#    #+#             */
-/*   Updated: 2024/11/21 14:10:17 by artuda-s         ###   ########.fr       */
+/*   Updated: 2024/12/10 19:26:30 by artuda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,20 @@
 #define REPLACE 3
 #define APPEND  4
 
+
+static int	ft_check_replace(char *try, t_envp *envp)
+{
+	if (!try || !*try || !envp)
+		return (false);
+	while (envp != NULL)
+	{
+		if (!strcmp(try, envp->key))
+			return (true);
+		envp = envp->next;
+	}
+	return (false);
+}
+
 static int	ft_replace_value(t_envp **head, char *key, char *value, int mode)
 {
 	t_envp	*cur;
@@ -24,7 +38,7 @@ static int	ft_replace_value(t_envp **head, char *key, char *value, int mode)
 	cur = *head;
 	while (ft_strcmp(key, cur->key) != 0)
 		cur = cur->next;
-	if (mode == REPLACE)
+	if (value && mode == REPLACE)
 	{
 		if (cur->value)
 			free(cur->value);
@@ -45,7 +59,7 @@ int	ft_add_entry_env(t_envp **head, char *key, char *value, int mode)
 {
 	t_envp	*temp;
 
-	if (ft_has_key(key, ft_strlen(key), *head))
+	if (ft_check_replace(key, *head))
 		ft_replace_value(head, key, value, mode);
 	else
 	{
@@ -73,7 +87,7 @@ int	ft_add_entry_env2(t_envp **head, char *key, char *value, int mode)
 	t_envp	*new;
 	t_envp	*cur;
 
-	if (ft_has_key(key, ft_strlen(key), *head))
+	if (ft_check_replace(key, *head))
 		ft_replace_value(head, key, value, mode);
 	else
 	{
